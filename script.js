4760f6fc58e4132c4e2cee1d8620f2aa19988b05
87973 <87973@glr.nl>
2023-01-26 01:50:27 +0100

    pixel images added

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let layer = document.querySelectorAll(".layer");
 cont.onscroll = function(){
     let X = cont.scrollTop;
 
-    layer[0].style.left = X + "px";
+    layer[0].style.left = X/4 + "px";
     layer[1].style.left = X/2 + "px";
-    layer[2].style.left = X/4 + "px";
 }
