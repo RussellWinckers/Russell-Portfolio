fdff2d08ad1a98eb92bcd0b4fc29e7f1638f97f8
87973 <87973@glr.nl>
2023-01-30 23:10:09 +0100

    character movement

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cont = document.querySelector(".outer-container");
+let cont = document.querySelector("#outer-container");
 
 let layer = document.querySelectorAll(".layer");
 
@@ -8,3 +8,25 @@ cont.onscroll = function(){
     layer[0].style.left = X/4 + "px";
     layer[1].style.left = X/2 + "px";
 }
+
+
+
+
+
+
+// //////////////////////////////////////////////////////////////////////// player movement
+
+let countScroll = 1;
+    
+    document.getElementById("outer-container").onscroll = function(){
+        if(countScroll = 1){
+        document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.4s steps(2) infinite";
+        (countScroll++);
+        console.log(countScroll);
+    }
+    setTimeout (function(){
+    if(countScroll = 2){
+        document.getElementById("character_spritesheet").style.animation = "none";
+        (countScroll = 1);
+    }}, 800);
+};
