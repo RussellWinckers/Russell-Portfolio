a828912ba3dffc0760a6b29d04b7598644163462
87973 <87973@glr.nl>
2023-01-31 18:30:19 +0100

    text added

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ cont.onscroll = function(){
 
     layer[0].style.left = X/4 + "px";
     layer[1].style.left = X/2 + "px";
+
+    if(countScroll = 1){
+                document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.5s steps(2) infinite";
+                (countScroll++);
+                console.log(countScroll);
+            }
+            setTimeout (function(){
+            if(countScroll = 2){
+                document.getElementById("character_spritesheet").style.animation = "none";
+                (countScroll = 1);
+            }}, 1000);
 }
 
 
@@ -16,17 +27,17 @@ cont.onscroll = function(){
 
 // //////////////////////////////////////////////////////////////////////// player movement
 
-let countScroll = 1;
+// let countScroll = 1;
     
-    document.getElementById("outer-container").onscroll = function(){
-        if(countScroll = 1){
-        document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.5s steps(2) infinite";
-        (countScroll++);
-        console.log(countScroll);
-    }
-    setTimeout (function(){
-    if(countScroll = 2){
-        document.getElementById("character_spritesheet").style.animation = "none";
-        (countScroll = 1);
-    }}, 1000);
-};
+//     document.getElementById("outer-container").onscroll = function(){
+//         if(countScroll = 1){
+//         document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.5s steps(2) infinite";
+//         (countScroll++);
+//         console.log(countScroll);
+//     }
+//     setTimeout (function(){
+//     if(countScroll = 2){
+//         document.getElementById("character_spritesheet").style.animation = "none";
+//         (countScroll = 1);
+//     }}, 1000);
+// };
