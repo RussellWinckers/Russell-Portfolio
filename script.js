2b363ecebf6d33b36eddfbc5cd482d6828ff57e6
Russell Winckers <russellwinckers@gmail.com>
2023-02-01 16:58:09 +0100

    added portal 1

--- script.js
+++ script.js
@@ -2,48 +2,42 @@ let cont = document.querySelector("#outer-container");
 
 let layer = document.querySelectorAll(".layer");
 
-setInterval (function(){
-           {
 userHasScrolled = false;
 
 cont.onscroll = function(){
+    
     let X = cont.scrollTop;
 
     layer[0].style.left = X/2 + "px";
     layer[1].style.left = X/1.5 + "px";
 
-           userHasScrolled = true;
-                    if(userHasScrolled) 
-                {
-                document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.5s steps(2) infinite";
-                userHasScrolled = false;
-                }
-                else{
-                    document.getElementById("character_spritesheet").style.animation = "none";
-                }
-                };
-}
-            }, 10);
-
-
-
-
-
-
 
 // //////////////////////////////////////////////////////////////////////// player movement
+userHasScrolled = true;
+                    setTimeout(() => {
+                userHasScrolled = false;
+                document.getElementById("character_spritesheet").style.animation = "none";
+                console.log("ho");
+                    }, 500); 
+                {
+                document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.4s steps(2) infinite";
+                }};
 
-// let countScroll = 1;
+
+let welkom = document.getElementById("welcomeText");
+
+
+
+
+
+// Show the website on button click
+welkom.onclick = function(){
+   document.getElementById("nameText").style.bottom = "500px";
+   welkom.style.display = "none";
+   setTimeout(() => {
+    document.getElementById("character").style.bottom = "170px";
+   cont.style.overflowY = "scroll";
+   }, 800);
+   
+};
     
-//     document.getElementById("outer-container").onscroll = function(){
-//         if(countScroll = 1){
-//         document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.5s steps(2) infinite";
-//         (countScroll++);
-//         console.log(countScroll);
-//     }
-//     setTimeout (function(){
-//     if(countScroll = 2){
-//         document.getElementById("character_spritesheet").style.animation = "none";
-//         (countScroll = 1);
-//     }}, 1000);
-// };
