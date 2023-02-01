b7d7e04dd72c0b3bf489d41f426122343428c898
Russell Winckers <russellwinckers@gmail.com>
2023-02-01 12:13:29 +0100

    charater changes

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ let cont = document.querySelector("#outer-container");
 
 let layer = document.querySelectorAll(".layer");
 
+setInterval (function(){
+           {
+userHasScrolled = false;
+
 cont.onscroll = function(){
     let X = cont.scrollTop;
 
-    layer[0].style.left = X/4 + "px";
-    layer[1].style.left = X/2 + "px";
+    layer[0].style.left = X/2 + "px";
+    layer[1].style.left = X/1.5 + "px";
 
-    if(countScroll = 1){
+           userHasScrolled = true;
+                    if(userHasScrolled) 
+                {
                 document.getElementById("character_spritesheet").style.animation = "moveSpritesheet 0.5s steps(2) infinite";
-                (countScroll++);
-                console.log(countScroll);
-            }
-            setTimeout (function(){
-            if(countScroll = 2){
-                document.getElementById("character_spritesheet").style.animation = "none";
-                (countScroll = 1);
-            }}, 1000);
+                userHasScrolled = false;
+                }
+                else{
+                    document.getElementById("character_spritesheet").style.animation = "none";
+                }
+                };
 }
+            }, 10);
+
 
 
 
